3f82ca0aebc69a42233ddb599ede0473e715c89e
CodeArc13 <codearc13@gmail.com>
2020-10-04 20:47:45 +0100

    minor file experimentation

--- PyDWMG.py
+++ PyDWMG.py
@@ -10,11 +10,14 @@
 
 def main():
     with open('D:\Games\EQLite\Logs\eqlog_Cleri_P1999Green.txt', 'r') as f:
-        #read_data = f.read()
-            #?set last read length to be the very end of the log
+        #f.seek(0, 2) #go to end of file #set last read length to be the very end of the log
+        #read_data = f.readline()
+        #print(read_data)
+        print(f.tell())
         for line in f:
             print(line, end='')
-            print(f.tell())
+        #    print(f.tell())
+        print(f.tell())
         #f.closed #not needed for with block
     pass
 
