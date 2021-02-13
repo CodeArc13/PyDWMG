243701984ca5f3ea3590811a6ff7e4a3f11aa3f5
Matt Lakin <49107138+mmlakin@users.noreply.github.com>
2021-02-13 01:04:23 -0800

    Working cross-thread comms
    Fixed log parser loop and implemented cross-thread comms by passing
    parentsignals to worker thread.  POC shows log parser thread status
    every 2 seconds so you can see when it's stopped.

--- PyQtDWMG.py
+++ PyQtDWMG.py
@@ -1,5 +1,5 @@
-import pdb
 import re
+import sys
 import time
 from PyQt5.QtWidgets import (
     QApplication,
@@ -14,6 +14,7 @@ from PyQt5.QtCore import (
     QObject,
     QRunnable,
     QThreadPool,
+    QTimer,
     pyqtSlot,
     pyqtSignal,
 )
@@ -21,27 +22,11 @@ from PyQt5.QtCore import (
 import time
 
 
-def follow(thefile):
-    thefile.seek(0, 2)  # Go to the end of the file
-    while True:
-        line = thefile.readline()
-        if not line:
-            time.sleep(0.1)  # Sleep briefly
-            continue
-        yield line
-
-
 class WorkerSignals(QObject):
     """
     Defines the signals available from a running worker thread.
 
     Supported signals are:
-
-    finished
-        No data
-
-    error
-        tuple (exctype, value, traceback.format_exc() )
 
     result
         object data returned from processing, anything
@@ -51,7 +36,11 @@ class WorkerSignals(QObject):
     result = pyqtSignal(object)
 
 
-class Worker(QRunnable):
+class ParentSignals(QObject):
+    terminate = pyqtSignal()
+
+
+class EQLogParser(QRunnable):
     """
     Worker thread
 
@@ -65,21 +54,51 @@ class Worker(QRunnable):
 
     """
 
-    def __init__(self, fn, *args, **kwargs):
-        super(Worker, self).__init__()
+    def __init__(self, parent_signals, *args, **kwargs):
+        super(EQLogParser, self).__init__()
         # Store constructor arguments (re-used for processing)
-        self.fn = fn
         self.args = args
         self.kwargs = kwargs
         self.signals = WorkerSignals()
-        self.kwargs["result_callback"] = self.signals.result
+        self.parent_signals = parent_signals
+        self._stopped = False
+        self.parent_signals.terminate.connect(self.stop)
+        self.show_status = QTimer()
+        self.show_status.timeout.connect(self.parser_status)
+        self.show_status.start(2000)
+
+    def __del__(self):
+        self._stopped = True
+
+    def stop(self):
+        self._stopped = True
+
+    def parser_status(self):
+        print("Log parsing thread active...")
 
     @pyqtSlot()
     def run(self):
-        """
-        Initialise the runner function with passed args, kwargs.
-        """
-        self.fn(*self.args, **self.kwargs)
+        # logfile = r"D:\Games\EQLite\Logs\eqlog_Cleri_P1999Green.txt"  # 'r' makes it raw, no need for \\ escapes, thanks!
+        logfile_path = "/home/mlakin/opt/storage/LutrisGames/everquest/Sony/EverQuest/Logs/eqlog_Pescetarian_P1999Green.txt"
+        zone_pattern = re.compile(r"^\[.*\] You have entered ([\w\s']+)\.$")
+        loc_pattern = re.compile(
+            r"^\[.*\] Your Location is (\-?\d+\.\d+), (\-?\d+\.\d+), (\-?\d+\.\d+)$"
+        )
+        print("starting timer...")
+        logfile = open(logfile_path, "rt")
+        with open(logfile_path, "rt") as logfile:
+            logfile.seek(0, 2)  # Go to the end of the file
+            while not self._stopped:
+                line = logfile.readline()
+                if not line:
+                    time.sleep(0.1)  # Sleep briefly
+                    continue
+                try:
+                    x, y, z = loc_pattern.findall(line)[0]
+                    # print(f"x: {x} y: {y} z: {z}") # Log loc to console
+                    self.signals.result.emit(f"x: {x} y: {y} z: {z}")
+                except IndexError:
+                    pass
 
 
 class MainWindow(QMainWindow):
@@ -91,9 +110,12 @@ class MainWindow(QMainWindow):
         self.l = QLabel("Start")
         b = QPushButton("Quit")
         b.pressed.connect(self.quit_app)
+        self.t = QPushButton("Terminate Log Parser")
+        self.t.pressed.connect(self.terminate_logparser)
 
         layout.addWidget(self.l)
         layout.addWidget(b)
+        layout.addWidget(self.t)
 
         w = QWidget()
         w.setLayout(layout)
@@ -109,39 +131,24 @@ class MainWindow(QMainWindow):
 
         self.start_workers()
 
-    def check_logfile(self, result_callback):
-        # logfile = r"D:\Games\EQLite\Logs\eqlog_Cleri_P1999Green.txt"  # 'r' makes it raw, no need for \\ escapes, thanks!
-        logfile_path = "/home/mlakin/opt/storage/LutrisGames/everquest/Sony/EverQuest/Logs/eqlog_Pescetarian_P1999Green.txt"
-        zone_pattern = re.compile(r"^\[.*\] You have entered ([\w\s']+)\.$")
-        loc_pattern = re.compile(
-            r"^\[.*\] Your Location is (\-?\d+\.\d+), (\-?\d+\.\d+), (\-?\d+\.\d+)$"
-        )
-        logfile = open(logfile_path, "rt")
-        for line in follow(logfile):
-            try:
-                x, y, z = loc_pattern.findall(line)[0]
-                print(f"x: {x} y: {y} z: {z}")
-                result_callback.emit(f"x: {x} y: {y} z: {z}")
-            except IndexError:
-                pass
-
     def update_label(self, text):
         self.l.setText(text)
 
+    def terminate_logparser(self):
+        self.logparser_control.terminate.emit()
+        self.t.setDisabled(True)
+
     def start_workers(self):
-        worker_logfile = Worker(self.check_logfile)
-        worker_logfile.signals.result.connect(self.update_label)
-        self.threadpool.start(worker_logfile)
+        self.logparser_control = ParentSignals()
+        self.worker_logfile = EQLogParser(self.logparser_control)
+        self.worker_logfile.signals.result.connect(self.update_label)
+        self.threadpool.start(self.worker_logfile)
 
     def quit_app(self):
-        self.threadpool.waitForDone(msecs=1000)
-        app.quit()
-
-    # self.l.setText(self.message)
-    # worker = Worker()
-    # self.threadpool.start(worker)
+        self.terminate_logparser()
+        sys.exit(app.quit())
 
 
 app = QApplication([])
 window = MainWindow()
-app.exec_()
+sys.exit(app.exec())
